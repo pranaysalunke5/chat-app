f58efb4ec9796d8da1bafc2e96bd0a5726e1d5dd
Pranay <pranaysalunke5@gmail.com>
2024-08-23 10:12:31 +0530

    user

---
+++ src/components/list/userInfo/Userinfo.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Userinfo() {
+  return (
+    <View>
+      <Text>Userinfo</Text>
+    </View>
+  )
+}
