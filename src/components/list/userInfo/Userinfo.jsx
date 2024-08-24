e16ad480ba225fa365e8c30484d32ee68cb5eba1
Pranay <pranaysalunke5@gmail.com>
2024-08-24 17:11:02 +0530

    user

--- src/components/list/userInfo/Userinfo.jsx
+++ src/components/list/userInfo/Userinfo.jsx
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function Userinfo() {
+const List = () => {
   return (
-    <View>
-      <Text>Userinfo</Text>
-    </View>
+    <div>
+      Hii
+      
+    </div>
   )
 }
+
+export default List
