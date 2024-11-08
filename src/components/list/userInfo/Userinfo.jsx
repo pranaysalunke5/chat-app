9c3d94535fe4c5dabd27eb818913a43d829f122a
Pranay <pranaysalunke5@gmail.com>
2024-11-08 10:00:45 +0530

    userInfo

--- src/components/list/userInfo/Userinfo.jsx
+++ src/components/list/userInfo/Userinfo.jsx
@@ -2,19 +2,19 @@ import "./userInfo.css";
 import { useUserStore } from "../../../lib/userStore";
 
 const Userinfo = () => {
-  // const { currentUser } = useUserStore();
+  const { currentUser } = useUserStore();
 
   return (
     <div className="userInfo">
       <div className="user">
-        {/* <img src={currentUser.avatar || "img/avatar.png"} alt="" /> */}
-        {/* <h2>{currentUser.username}</h2> */}
+        <img src={currentUser.avatar || "img/avatar.png"} alt="User Avatar" />
+        <h2>{currentUser.username}</h2>
       </div>
-      {/* <div className="icons">
-        <img src="img/more.png" alt="" />
-        <img src="img/video.png" alt="" />
-        <img src="img/edit.png" alt="" />
-      </div> */}
+      <div className="icons">
+        <img src="img/more.png" alt="More" />
+        <img src="img/video.png" alt="Video" />
+        <img src="img/edit.png" alt="Edit" />
+      </div>
     </div>
   );
 };
