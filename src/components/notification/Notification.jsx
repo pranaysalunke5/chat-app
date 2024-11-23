28157b82764adf87b6f6637c35438ed74f6ae76e
Pranay <pranaysalunke5@gmail.com>
2024-11-23 10:19:20 +0530

    notifications

--- src/components/notification/Notification.jsx
+++ src/components/notification/Notification.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './Notification.css';
 
 const Notification = () => {
   return (
-    <div>
-      
+    <div className="notification-container">
+      <div className="notification-header">
+        Notifications
+      </div>
+      <div className="notification-list">
+        <div className="notification-item">
+          <p className="notification-message">You have a new message!</p>
+          <span className="notification-time">2 mins ago</span>
+        </div>
+        <div className="notification-item">
+          <p className="notification-message">Your friend joined the chat.</p>
+          <span className="notification-time">5 mins ago</span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Notification
+export default Notification;
